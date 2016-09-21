e9e7ce9188c22583a98c64246a7e13a1f2ca7133
George <gkorepanov.gk@gmail.com>
2016-09-21 15:55:44 +0300

    Checkpoint

--- code/MIPT_Plot.py
+++ code/MIPT_Plot.py
@@ -2,15 +2,11 @@ from numpy import *
 import Gnuplot, Gnuplot.funcutils, os, pyperclip, sys
 
 
-class C_Data(object):
-	pass
 
 class C_Plot(object):
 	def __init__ (self):
 		self.filename = None
-		self.xscale = 1.3
-		self.yscale = 1.1
-		self.tempname = "temp.dat"
+		self.tempname = None
 		self.xerror = None
 		self.yerror = None
 		self.xtitle = None
@@ -23,7 +19,7 @@ class C_Plot(object):
 		self.labelx = None
 		self.labely = None
 		self.labeltext = None
-		self.fontsize = "15"
+		self.fontsize = "22"
 		self.term = "epslatex"
 
 		self.xmax = 0
@@ -32,77 +28,71 @@ class C_Plot(object):
 		self.fit = 1
 		self.extra = ""
 
-		self.number = 1
 
 
 
-#global variable with all settings, TODO: make local
-plot = C_Plot()
 
 # TODO: revise
-def loadfile():
-	file = open(plot.filename, "r")
+def load(self):
+	file = open(self.filename, "r")
 
 	for line in file:
 		items = line.rstrip("\n").split(' ')
 		items[0] = items[0][0:-1]
-		print items
+		#print items
 		if items[0] == "x_error":
-			plot.xerror = int(items[1])
+			self.xerror = int(items[1])
 		elif items[0] == "y_error":
-			plot.yerror = int(items[1])
+			self.yerror = int(items[1])
 		elif items[0] == "output":
-			plot.output = items[1]
+			self.output = items[1]
 
 		elif items[0] == "x_title":
-			plot.xtitle = ' '.join(items[1:])
+			self.xtitle = ' '.join(items[1:])
 		elif items[0] == "y_title":
-			plot.ytitle = ' '.join(items[1:])
+			self.ytitle = ' '.join(items[1:])
 		elif items[0] == "x_limits":
-			plot.xlimits = '['+items[1]+':'+items[2]+']'
-			plot.xmax = int(items[2])
+			self.xlimits = '['+items[1]+':'+items[2]+']'
+			self.xmax = int(items[2])
 		elif items[0] == "y_limits":
-			plot.ylimits = '['+items[1]+':'+items[2]+']'
-			plot.ymax = int(items[2])
+			self.ylimits = '['+items[1]+':'+items[2]+']'
+			self.ymax = int(items[2])
 
 		elif items[0] == "label":
-			plot.label = items[1]
+			self.label = items[1]
 		elif items[0] == "label_x":
-			plot.labelx = items[1]
+			self.labelx = items[1]
 		elif items[0] == "label_y":
-			plot.labely = items[1]
+			self.labely = items[1]
 		elif items[0] == "label_text":
-			plot.labeltext = ' '.join(items[1:]).strip('"')
+			self.labeltext = ' '.join(items[1:]).strip('"')
 		elif items[0] == "font_size":
-			plot.fontsize = items[1]
+			self.fontsize = items[1]
 		elif items[0] == "term":
-			plot.term = ' '.join(items[1:])
+			self.term = ' '.join(items[1:])
 
 
 		elif items[0] == "extra":
-			plot.extra = plot.extra + ' '.join(items[1:])+'\n'
+			self.extra = self.extra + ' '.join(items[1:])+'\n'
 
 		elif items[0] == "fit":
-			plot.fit = int(items[1])
-		elif items[0] == "plots_number":
-			plot.number = int(items[1])
+			self.fit = int(items[1])
 
 		elif items[0] == "data":
 			break
 		else: 
 			pass
 
-	print "SEE", plot.fit
-	proceed(file)
+	self.copydata(file)
 	file.close()
 
 # TODO: revise and join with previous
-def proceed(file):
-	if not plot.output:
+def copydata(self, file):
+	if not self.output:
 		print "Wrong file format"
 		sys.exit(1)
 
-	tempfile = open(plot.tempname, "w")
+	tempfile = open(self.tempname, "w")
 	for line in file:
 		if line == '\n' or not line or line[0] == '#':
 			pass
@@ -110,37 +100,63 @@ def proceed(file):
 			tempfile.write(line.replace(",", "."))
 	
 	tempfile.close()
+	file.close()
 
-	doplot()
+
+def setup_g(plot, g, preview):
+	if preview:
+		g('set term wxt size 1600,900 font "sansserif, '+\
+		plot.fontsize+'"')
+		g('set multiplot')
+
+	else:
+		g('unset multiplot')
+		g('set output "'+plot.output+'"')
+
+#TODO: just random values for A4 paper LaTeX. Give chance to change.
+		if plot.term == "epslatex":
+			g('set term epslatex size 16.5cm,10cm')
+			g('set format xy "$%g$"')
 
 
-def doplot():
-	g = Gnuplot.Gnuplot(debug=1)
-	if plot.fit:
+		else:
+			g('set term '+plot.term+' size 1600,900 font "sansserif, '+\
+			plot.fontsize+'"')
+
+		g('set multiplot')
+
+
+
+def do(self, g, preview, color=7):
+	if self.fit:
 		g('f(x)=a*x+b')
 # Using of temporary file. TODO.
 		g('fit f(x) "'+\
-			plot.tempname+\
+			self.tempname+\
 			'" using 1:2 via a,b')
 
 
 	g('unset key')
 # TODO: just random values. Make settings
-	g('set term wxt size 1600,900 font "sansserif, '+\
-		plot.fontsize+'"')
+	
 
 
-	if plot.xtitle:
-		g.xlabel(plot.xtitle)
-	if plot.ytitle:
-		g.ylabel(plot.ytitle)
+	if self.xtitle:
+		g.xlabel(self.xtitle)
+	if self.ytitle:
+		g.ylabel(self.ytitle)
 
+
+#set style line 1 lc rgb \'black\' pt 7 ps 1.2 lt 5 lw 3
+	#set style line 2 lc rgb \'black\' pt 7 ps 0 lt 5 lw 1
 
 # TODO: give chance to change style
+
+
 	g('''
 
-	set style line 1 lc rgb \'black\' pt 7 ps 1.2 lt 5 lw 3
-	set style line 2 lc rgb \'black\' pt 7 ps 0 lt 5 lw 1
+	set style line 1 lc ''' + str(color) + ''' pt 7 ps 1.2 lt 5 lw 3
+	set style line 2 lc ''' + str(color) + ''' pt 7 ps 0 lt 5 lw 1
 
 	set grid ytics lc rgb "#bbbbbb" lw 1 lt 0
 	set grid xtics lc rgb "#bbbbbb" lw 1 lt 0
@@ -150,81 +166,91 @@ def doplot():
 
 	set fit errorvariables
 
+	
 	''')
-	g(plot.extra)
 
-	if plot.xlimits:
-		g('set xrange ' + plot.xlimits)
-	if plot.ylimits:
-		g('set yrange ' + plot.ylimits)
+	g(self.extra)
+
+	if self.xlimits:
+		g('set xrange ' + self.xlimits)
+	if self.ylimits:
+		g('set yrange ' + self.ylimits)
 
 
 
-	if plot.label and plot.fit:
+	if self.label and self.fit:
 		s = 'set label sprintf('+\
-		('"$K = %.2f\\\\pm%.2f$' if plot.term == "epslatex" else '"K = %.2f+-%.2f')+\
-		(plot.labeltext if plot.labeltext else "")+\
+		('"$K = %.2f\\\\pm%.2f$' if self.term == "epslatex" else '"K = %.2f+-%.2f')+\
+		(self.labeltext if self.labeltext else "")+\
 		'",a, a_err)'
 
 
 
-		if plot.labelx and plot.labely:
-			s = s+' at '+str(plot.labelx)+','+\
-			str(plot.labely)
+		if self.labelx and self.labely:
+			s = s+' at '+str(self.labelx)+','+\
+			str(self.labely)
 		else:
-			s = s+' at '+str(plot.xmax*0.1)+','+\
-			str(plot.ymax*0.9)
+			s = s+' at '+str(self.xmax*0.1)+','+\
+			str(self.ymax*0.9)
 
 
 		g(s)
 
-	s1 =  ' "' + plot.tempname +'"'
+	s1 =  ' "' + self.tempname +'"'
 	s = 'plot ' + s1+' using 1:2 ls 1,' + s1
 
 
-	if plot.xerror and plot.yerror:
+	if self.xerror and self.yerror:
 		s = s+' using 1:2:4:3 ls 2 with xyerrorbars'
-	elif plot.xerror:
+	elif self.xerror:
 		s = s+' using 1:2:3 ls 2 with xerrorbars'
-	elif plot.yerror:
+	elif self.yerror:
 		s = s+' using 1:2:3 ls 2 with yerrorbars'
 	else:
 		s = s+' using 1:2 ls 2 with points'
 		
-	if plot.fit:
+	if self.fit:
 		s = s +	', f(x) ls 1 with lines'
 
 	g(s)
 
-	
 
-	
-	g('set output "'+plot.output+'"')
-
-#TODO: just random values for A4 paper LaTeX. Give chance to change.
-	if plot.term == "epslatex":
-		g('set term epslatex size 16.5cm,10cm')
-		g('set format xy "$%g$"')
-
-		#if plot.xscale and plot.yscale:
-			#g('set size '+str(plot.xscale)+','+str(plot.yscale))
-
-	else:
-		g('set term '+plot.term+' size 1600,900 font "sansserif, '+\
-		plot.fontsize+'"')
-
-	g('replot')
-
-	raw_input('')
-	os.remove(plot.tempname)
-
+C_Plot.do = do
+C_Plot.copydata = copydata
+C_Plot.load = load
 
 
 # TODO: everywhere, implement error checks
 if __name__ == "__main__":
-    if len (sys.argv) == 2:
-        plot.filename = sys.argv[1]
-        loadfile()
-    else:
-    	print "Using: __main__ [filename]"
-        sys.exit(1)
+
+	plots = []
+	i = 0
+
+	for argv in (sys.argv[1:]):
+		print argv
+		plot = C_Plot()
+		plot.filename = argv
+		plot.tempname = "temp"+str(i)+".dat"
+		plot.load()
+		plots.append(plot)
+		i=i+1
+
+	g = Gnuplot.Gnuplot(debug=1)
+	setup_g(plots[0], g, preview = 1)
+
+	j = 1
+	for plot in plots:
+		if i == 1:
+			plot.do(g, preview = 1)
+		else: 
+			plot.do(g, preview = 1, color=j)
+		j = j+1
+
+	raw_input(' ')
+
+	setup_g(plots[0], g, preview = 0)
+
+	i = j
+	for plot in plots:
+		plot.do(g, preview = 0, color=j)
+		j=j+1
